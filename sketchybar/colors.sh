0042992d95824a4359e5b91feb6d0a0ee4e458a4
scirons <gabriel@scirons.com>
2025-07-02 23:10:30 +0200

    kitty|sketchybar: ciapre & visa theme

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export WHITE=0xffffffff
+export WHITE=0xffada37a
 
-# -- Scirons Dark Scheme --
-export BAR_COLOR=0xff151718
-export ITEM_BG_COLOR=0xff1b1d1e
-export ACCENT_COLOR=0xff32383a
+# -- Visa Dark Scheme --
+export BAR_COLOR=0xff181c27
+export ITEM_BG_COLOR=0xff181c27
+export ACCENT_COLOR=0xff172539
 
-# -- Scirons Transparency Scheme --
+# -- Visa Transparency Scheme --
 #export BAR_COLOR=0xff151718
 #export ITEM_BG_COLOR=0xff1b1d1e
 #export ACCENT_COLOR=0xff32383a
